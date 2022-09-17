a5a6719191059474ba4116e478df11264f7892ef
Vagn99 <christoffer.lorenzen@outlook.com>
2022-09-17 13:36:06 +0200

    updateeee

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,18 +9,34 @@ import drippingCock from "../img/dripping-cock.jpg";
 const Home = () => {
   return (
     <Container>
-      <Row>
+      <Row className="mt-4">
         <Col>
         <div className="d-flex">
           <h1 className="mx-auto">Gruppe 6 er bedre end gruppe 1</h1>
         </div>
         </Col>
       </Row>
-      <Row>
-        <Col>
-        <div className="d-flex pt-5">
-          <img src={drippingCock} className="mx-auto" width="100%" alt="dripping cock" />
-        </div>
+      <Row style={{borderBottom: "5px solid red"}}>
+        <Col xs="12" md="6">
+          <div className="d-flex pt-5">
+            <img src={drippingCock} className="mx-auto" width="100%" alt="dripping cock" />
+          </div>
+        </Col>
+        <Col xs="12" md="6">
+          <div className="d-flex pt-5">
+            <p className="mx-auto">
+              Forskere har i mange år forsøgt at finde ud af, hvad der gør gruppe 6 til den bedste gruppe. De har fundet ud af, at det er de bare.
+              <br/>
+              <br/>
+              Ingen andre formår at nå deres niveau.
+              <br/>
+              <br/>
+              Og sådan er det.
+              <br/>
+              <br/>
+              Dryppende hane.
+            </p>
+          </div>
         </Col>
       </Row>
     </Container>
