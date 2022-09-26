cbde44609421c628b94270a5b34309fe1e6a7b06
Vagn99 <christoffer.lorenzen@outlook.com>
2022-09-26 12:28:22 +0200

    gg

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
+import {BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+
 import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          {/* <Route path="/signup" element={<Signup/>} />
+          <Route path="/profile" element={<Profile/>} />
+          <Route path="/police" element={<Police/>} />
+          <Route path="/customize" element={<Customize/>} /> */}
+        </Routes>
+      </Router>
+    </>
   );
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,23 +1,28 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import { Stack } from "react-bootstrap";
 
 import "./Home.css";
 
 import drippingCock from "../img/dripping-cock.jpg";
-import carl from "../img/carlll.jpg";
 
 const Home = () => {
   return (
     <Container>
+      <Row>
+        <Stack>
+          <div></div>
+        </Stack>
+      </Row>
       <Row className="mt-4">
         <Col>
         <div className="d-flex">
-          <h1 className="mx-auto">Gruppe 6 er bedre end gruppe 1</h1>
+          <h2 className="mx-auto">Gruppe 6 er bedre end gruppe 1</h2>
         </div>
         </Col>
       </Row>
-      <Row className="pb-5" style={{borderBottom: "5px solid red"}}>
+      <Row className="pb-5">
         <Col xs="12" md="6">
           <div className="d-flex pt-5">
             <img src={drippingCock} className="mx-auto" width="100%" alt="dripping cock" />
@@ -37,13 +42,6 @@ const Home = () => {
               <br/>
               Dryppende hane.
             </p>
-          </div>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <div className="d-flex pt-5">
-            <img src={carl} className="mx-auto d-block rotate" width="20%" alt="carl" />
           </div>
         </Col>
       </Row>
